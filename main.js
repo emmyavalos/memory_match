1e1e550ec9e0f2184c3ad7afa69bedea2321e7fb
Emmy Avalos <emmyavalos415@gmail.com>
2017-11-16 16:24:29 -0700

    memory match return timeout

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 
-$(document).ready( function() {
 
+$(document).ready( function() {
   $('img').attr('src', 'images/atari.png')
 
   $('img').on('click', function() {
     var source = $(this).attr('alt')
-    $(this).attr('src', 'images/' + source )
+  $(this).attr('src', 'images/' + source )
+  setTimeout(function() {
+    $('img').attr('src', 'images/atari.png')
+  }, 2000);
 
  })
-})
 
-//compare 1st card to 2nd card
-// setTimeout(function() {
-//       $("img").attr("src", "./resources/images/car.jpg")
-//     }, 1000);
+})
